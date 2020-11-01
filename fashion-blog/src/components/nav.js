cb326b59b6d8c9b6cf940121844363ccb9f9c23f
Amiinoz <momagan@gmail.com>
2020-10-31 21:37:19 -0600

    Initial setup

---
+++ fashion-blog/src/components/nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function Nav() {
+
+  return(
+    <div className="nav">
+      <span> Fashow</span>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  )
+
+}
+
+
+export default Nav;
